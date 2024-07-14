64ee6bdc305148f6291b80f72fb8d60e5e0627c9
kathf-create <173672929+kathf-create@users.noreply.github.com>
2024-07-14 19:46:18 +1000

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// js/scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('.menu-button');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuButton.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
